323e22a5bf797eb0f338c772d7192df01d6d0acf
Jitendra <yadavjitendra238@gmail.com>
2018-07-05 22:02:11 +0530

    first commit

--- oxford/settings.py
+++ oxford/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = '96kfcqumxjdo$5palui+208upb)n@w2ug-o(tr3%_8pd&n*&)&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-#ALLOWED_HOSTS = ['jeetu-dict.herokuapp.com']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jeetu-dict.herokuapp.com']
+#ALLOWED_HOSTS = []
 
 
 # Application definition
